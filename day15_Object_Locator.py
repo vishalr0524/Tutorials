2af13d9af1520b1eebe98cfcd94cf670dffe926e
vishalr0524 <vishal@dhvaniai.com>
2025-11-23 23:47:48 +0530

    Capstone: Object Locator

---
+++ day15_Object_Locator.py
@@ -0,0 +1,120 @@
+import cv2
+import numpy as np
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)  
+cap.set(4, frameHeight) 
+
+def empty(a):
+    pass
+cv2.namedWindow("Parameters")
+cv2.resizeWindow("Parameters", 640, 240)
+cv2.createTrackbar("Threshold1", "Parameters", 150, 255, empty)
+cv2.createTrackbar("Threshold2", "Parameters", 255, 255, empty)
+cv2.createTrackbar("Area", "Parameters", 5000, 30000, empty)
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(rows):
+            for y in range(cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor) 
+        return ver  
+    else:
+        for x in range(rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+        return ver
+
+def getShapeName(approx, area, peri):
+    points = len(approx)
+    
+    if points == 3:
+        return "Triangle"
+    
+    if points == 4:
+        x, y, bw, bh = cv2.boundingRect(approx)
+        if bh == 0:
+             return "Rectangle" 
+        aspect_ratio = bw / float(bh)
+        if 0.95 <= aspect_ratio <= 1.05:
+            return "Square"
+        return "Rectangle"
+    if peri > 0:
+        circularity = 4 * np.pi * area / (peri * peri)
+        if circularity > 0.85:
+            return "Circle"
+    
+    return "Polygon"
+
+def getContours(img, imgContour):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(imgContour, contours, -1, (255, 0, 255), 7)
+
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        areaMin = cv2.getTrackbarPos("Area","Parameters")
+        if area > areaMin:
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 255), 7)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            objectType = getShapeName(approx, area, peri)
+            # print(len(approx))
+            x, y, w, h = cv2.boundingRect(approx)
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 5)
+
+            cv2.putText(imgContour, objectType, (x + w + 20, y + 20), cv2.FONT_HERSHEY_COMPLEX, .7,
+                        (0, 255, 0), 2)
+            cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX, 0.7,
+                        (0, 255, 0), 2)
+
+
+while True:
+    cap.grab()                         
+    success, img = cap.retrieve()    
+
+    if not success:
+        print("Error: Failed to retrieve frame.")
+        break
+
+    imgContour = img.copy()
+    imgBlur = cv2.GaussianBlur(img, (7, 7), 1)
+    imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+
+    threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+    threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+    imgCanny = cv2.Canny(imgGray, threshold1, threshold2)
+    kernel = np.ones((5, 5))
+    imgDil = cv2.dilate(imgCanny, kernel, iterations=1)
+
+    getContours(imgDil, imgContour)
+
+    imgStack = stackImages(0.8, ([img, imgGray, imgCanny],[imgDil, imgContour, imgDil]))
+    cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
+    cv2.imshow("Result", imgStack)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
